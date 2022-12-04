baf970ae45955465d89fee694fafd06acbffa2fa
GuilhermeNPCosta <guilherme.costa@sptech.school>
2022-12-04 04:24:54 -0300

    Alterações em javascript,´páginas de cadastro, login, inicial, área de usuário, etc.

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "SEU_DATABASE",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    database: "Projeto_PI",
+    user: "root",
+    password: "SptechAluno!",
 };
 
 function executar(instrucao) {
